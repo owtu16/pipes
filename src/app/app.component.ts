384f6dc5265d15ae55f663eb48c657863f17c3d2
Melvin Cancino <melvin.cancino88@gmail.com>
2020-05-17 16:40:36 -0700

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name: string;
+  Paymentdate: string;
+  amount: number;
+  height: number;
+  miles: number;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+
+  }
 
   onNameChange(value:string){
     this.name = value;
+  }
+  onDateChange(value:string){
+    this.Paymentdate = value;
+  }
+
+  onAmountChange(value:string){
+    const parsedValue = parseFloat(value);
+
+    if(!isNaN(parsedValue)){
+      this.amount = parsedValue;
+    }
+
+  }
+
+  onHeightChange(value: string){
+    this.height = parseFloat(value);
+  }
+
+  onMilesChange(value: string){
+    this.miles = parseFloat(value);
   }
 }
